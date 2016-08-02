38346a036b3caf8b210e4d6d5adede73e32833c4
Romain Dorgueil <romain@dorgueil.net>
2016-08-02 13:59:08 +0200

    update docker run so it works with native osx docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --name graphite -v /data/graphite:/data -e SECRET_KEY='random-secret-key' -p 80:80 -p 3000:3000 -p 2003:2003 -p 2004:2004 -p 7002:7002 -p 8125:8125/udp -p 8126:8126 -d rdorgueil/graphite
+docker run --name graphite -v $PWD/volumes/graphite:/data -e SECRET_KEY='random-secret-key' -p 80:80 -p 3000:3000 -p 2003:2003 -p 2004:2004 -p 7002:7002 -p 8125:8125/udp -p 8126:8126 -d rdorgueil/graphite
